4e33f98a11afc9515dc62c491811097602cb8fcc
Makabi Yelmis <gyelmis@gmail.com>
2020-03-19 08:34:18 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,7 @@ Route::group(['prefix' => 'auth'], function () {
     Route::group(['middleware' => ['auth:api']], function () {
         Route::post('update', 'AuthController@update');
         Route::get('show', 'AuthController@show');
-         Route::post('update-password', 'UserController@updatePassword');
+         Route::post('update-password', 'UserController@changePassword');
         
     });
 });
