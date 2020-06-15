6b397c0495b7f64e3b3549a9cf34538cc0f3d275
Makabi Yelmis <gyelmis@gmail.com>
2020-06-15 22:54:50 +0100

    Update app.php

--- config/app.php
+++ config/app.php
@@ -55,6 +55,8 @@ return [
     'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
+    
+    'ps_key' => env('PS_KEY', ''),
 
     /*
     |--------------------------------------------------------------------------
